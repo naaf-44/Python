4247743030d23d226569b02ceabfd466eb91f672
naaf-44 <75825890+naaf-44@users.noreply.github.com>
2021-03-16 16:45:39 +0530

    Update whatsAppChat.py

--- whatsAppChat.py
+++ whatsAppChat.py
@@ -1,2 +1,8 @@
 import pywhatkit
+
+#parameters
+# 1. mobile number of the reciever
+# 2. message to send
+# 3. hour in 24 hour formate
+# 4. minutes
 pywhatkit.sendwhatmsg("+91807348****","Hello",14,40)
